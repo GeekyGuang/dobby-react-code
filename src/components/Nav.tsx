7bc9e4f6f2226e772fa6d03c837a3ff328ac9838
travis <travis.x.wong@gmail.com>
2021-09-22 18:29:21 +0800

    使用svg-sprite-loader

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,27 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
 
 const NavWrapper = styled.nav`
-  line-height: 24px;
+ line-height: 24px;
   box-shadow: 0 0 3px rgba(0,0,0,0.2);
   > ul {
     display: flex;
     > li {
       width: 33.3333%;
       text-align: center;
-      padding: 16px;
+      padding: 4px 0;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      .icon {
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `
@@ -20,12 +31,21 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#label"/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#money" />
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="icon">
+            <use xlinkHref="#statistics" />
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
