712ba336b57526ac2d5b434b173bbc08ec208df2
travis <travis.x.wong@gmail.com>
2021-10-05 23:44:28 +0800

    重构onChange

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,7 +5,6 @@ import { TagsSection } from 'components/Money/TagsSection';
 import { NoteSection } from 'components/Money/NoteSection';
 import { CategorySection } from 'components/Money/CategorySection';
 import { NumberPadSection } from 'components/Money/NumberPadSection';
-import set = Reflect.set;
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -21,17 +20,22 @@ function Money() {
     note: '',
     amount: 0
   })
+  const onChange = (obj: Partial<typeof selected>) => {
+    setSelected({
+      ...selected,
+      ...obj
+    })
+  }
   return (
     <MyLayout>
-      {selected.amount}
       <TagsSection value={selected.tags}
-                   onChange={(tags: string[]) => setSelected({...selected, tags: tags})}/>
+                   onChange={tags => onChange({tags})}/>
       <NoteSection value={selected.note}
-                   onChange={(note: string) => setSelected({...selected, note: note})}/>
+                   onChange={note => onChange({note})}/>
       <CategorySection value={selected.category}
-                       onChange={(category: '-'|'+') => setSelected({...selected, category: category})}/>
+                       onChange={category => onChange({category})}/>
       <NumberPadSection value={selected.amount}
-                        onChange={(amount: number) => setSelected({...selected, amount: amount})}/>
+                        onChange={amount => onChange({amount})}/>
     </MyLayout>
   );
 }
