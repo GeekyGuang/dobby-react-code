80875f07abf62247a8214395a5e31e1fa4d127c0
travis <travis.x.wong@gmail.com>
2021-10-07 22:32:41 +0800

    使用useParams获取路由参数

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import Layout from '../components/Layout';
+import {useTags} from '../useTags';
 
+type Params = {
+  id: string;
+}
 const Tag:React.FC = () => {
+  const {findTag} = useTags()
+  const {id} = useParams<Params>()
+  const tag = findTag(parseInt(id))
   return (
-    <Layout>hi</Layout>
+    <Layout>{tag.name}</Layout>
   )
 }
 
